4ecf12accd884e5ffd7f5c618fb2a6118f652601
rookie-leo <proleonardopa@gmail.com>
2024-01-14 12:21:35 -0300

    [ build ]Adicionado validações com Spring Validation

--- src/main/kotlin/com/mercadolivro/http/controllers/request/CreateCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/http/controllers/request/CreateCustomerRequest.kt
@@ -1,6 +1,12 @@
 package com.mercadolivro.http.controllers.request
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+
 data class CreateCustomerRequest(
+    @field:NotEmpty
     var name: String,
+
+    @field:Email
     var email: String
 )
